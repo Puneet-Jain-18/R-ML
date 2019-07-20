cc01241bc608fb04acd58f45df9dc6126cc6b25b
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-07-20 23:35:20 +0530

    k means complete

--- netflix (K-means clustering).R
+++ netflix (K-means clustering).R
@@ -6,16 +6,15 @@ movie <- data.matrix(movie_metadata)
 movie <- na.omit(movie)
 
 #se;ecting sample 500 values
-smple <- movie[sample(nrow(movie),500)]
+smple <- movie[sample(nrow(movie),500),]
 
-smple_sort <-smple[c(9,23)]
-print (smple_sort)
+smple_sort <-smple[,c(9,23)]
 
 smple_matrix <-data.matrix(smple_sort)
 
 #elbow curve 
 wss <- (nrow(smple_matrix-1)*sum(apply(smple_matrix, 2, var)))
-for (i in 2:5) wss[i] <-sum (kmeans(smple_matrix,centers = 2)$withness)
+for (i in 2:15) wss[i] <-sum (kmeans(smple_matrix,centers = i)$withinss)
 plot (1:15 ,wss, type= "b", xlab = "number of clusters", ylab = "within sum of squawre")
 
 
