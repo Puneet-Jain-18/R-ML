0ca5a7de07b15eb00c438c1b1715b2183952fd91
Puneet-Jain-18 <puneetjain718@gmail.com>
2019-07-24 01:06:59 +0530

    decision tree

---
+++ dibaties(Decision tree).r
@@ -0,0 +1,24 @@
+dibaet <- read.csv("./datafiles/dibaties.csv")
+library(caTools)
+split <- sample.split(dibaet , SplitRatio = 0.8);
+
+training<- subset(dibaet,split ==TRUE);
+
+testing <-subset(dibaet, split ==FALSE);
+
+library(rpart)
+
+model <- rpart(type~. , data= training )
+print(model)
+
+plot(model,margin=0.1)
+text(model,use.n=TRUE,pretty=TRUE)
+
+
+##Now we would make prediction
+
+pred <- predict(model,newdata=testing,type="class")
+
+## so we build a confusion matrix to verify with the actual data
+library(caret)
+print(confusionMatrix(table(actualValue= testing$type, predValue = pred)))
